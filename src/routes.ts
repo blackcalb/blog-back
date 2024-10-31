d88c24fd7de7b3c730ad2cc20146104302ad4c9a
Nelson Varas U <odilon.varas@gmail.com>
2024-10-31 13:46:42 +0100

    add dummy endpoint

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
-import { Application } from "express";
+import { Application, Request, Response } from "express";
 import { authRouter } from "./router/auth";
 import bodyParser from "body-parser";
 import AuthMiddleware from "./middlewares/auth";
@@ -8,6 +8,9 @@ import { postRouter } from "./router/posts";
 export default function routes(app: Application) {
   const jsonParser = bodyParser.json();
 
+  app.get("/", (req: Request, res: Response) => {
+    res.send("👋");
+  });
   app.use("/auth", jsonParser, authRouter);
   app.use("/user", jsonParser, AuthMiddleware, userRouter);
   app.use("/api/posts", jsonParser, postRouter);
